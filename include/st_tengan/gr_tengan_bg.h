ed1574cf31ad0ffc314258da29e2a727dad92786
MarioFan64 <LovingMario64@gmail.com>
2026-06-03 14:10:32 -0500

    match and link Spear Pillar ground objects

---
+++ include/st_tengan/gr_tengan_bg.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <gr/gr_yakumono.h>
+#include <st_tengan/gr_tengan.h>
+
+class grTenganBg : public grTengan {
+protected:
+    Vec3f *posDialgaWork;
+    Vec3f *posAshibaWork;
+
+public:
+    grTenganBg(const char* taskName);
+    virtual ~grTenganBg();
+    virtual void update(float deltaFrame);
+    virtual void setPosDialgaWork(Vec3f* pos);
+    virtual void setPosAshibaWork(Vec3f* pos);
+
+    static grTenganBg* create(int mdlIndex, const char* tgtNodeName, const char* taskName);
+
+};
